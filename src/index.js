757fcee6623a32fac5b84496167e11c9bfa52ca1
MikeSivak <noizemcnorm@gmail.com>
2021-05-25 18:47:09 +0300

    header created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header.js';
+import './styles/fonts.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header/>
   </React.StrictMode>,
   document.getElementById('root')
 );
